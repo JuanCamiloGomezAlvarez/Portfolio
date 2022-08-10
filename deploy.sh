0d1b22838843695f6f1d37001012d22689a699f3
JuanCamiloGomezAlvarez <kamygz93@hotmail.com>
2022-08-09 21:59:30 -0500

    deploy.sh modicaded

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:JuanCamiloGomezAlvarez/Portfolio-Juan-Camilo-Gomez-Alvarez.git main:gh-pages
+git push -f git@github.com:JuanCamiloGomezAlvarez/Portfolio-Juan-Camilo-Gomez-Alvarez.git main:gh-pages
 
 cd -
