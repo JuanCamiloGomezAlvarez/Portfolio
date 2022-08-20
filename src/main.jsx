b8def75fcbac93d86bebb7e42452c1008db49aab
JuanCamiloGomezAlvarez <kamygz93@hotmail.com>
2022-08-20 11:55:14 -0500

    created component: ProjectCard

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from "./components/Header";
+import ProjectCard from './components/ProjectCard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
+    <ProjectCard/>
   </React.StrictMode>
 )
