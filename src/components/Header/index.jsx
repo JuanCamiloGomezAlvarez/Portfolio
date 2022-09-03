f65e0a51ff5aa0dabae9ab1253edb7329318f132
JuanCamiloGomezAlvarez <kamygz93@hotmail.com>
2022-09-03 18:33:52 -0500

    cambios en el header desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -17,26 +17,34 @@ export default function Header(){
 
     return(
         <header>
-        <div className="header">
-            <div className = "profile">
-                <a href="#" onClick={pictureToggle}><img className = "profile-img" src="https://avatars.githubusercontent.com/u/87502730?s=400&u=dd34bf3c999b627e50e3e3b35cc3f16209706fa3&v=4" alt="profile picture"/></a>
-                {/* <div className = "profile-greetings">
-                    <p>Hi! I'm Juan</p>
-                    <span><InteractiveText /></span>
-                </div> */}
+            <div className="header-container">
+                <div className="header">
+                    <div className = "profile">
+                        <a href="#" onClick={pictureToggle}><img className = "profile-img" src="https://avatars.githubusercontent.com/u/87502730?s=400&u=dd34bf3c999b627e50e3e3b35cc3f16209706fa3&v=4" alt="profile picture"/></a>
+                        {/* <div className = "profile-greetings">
+                            <p>Hi! I'm Juan</p>
+                            <span><InteractiveText /></span>
+                        </div> */}
+                    </div>
+
+                    <div className="header-icon-container">
+                        <IconSocialMedia/>
+                    </div>  
+                      
+                    <a href="#" className="menuNav" onClick={navToggle}><div></div><div></div><div></div></a>
+                </div>
+
+                <div className="toggle-nav" id="menuNav">
+                    <div className="menuNav-list" >
+                        <nav>
+                            <a href="/">Home</a>
+                            <a href="/">About</a>
+                            <a href="/">Projects</a>              
+                        </nav>               
+                    </div>
+                </div>
             </div>
-            <IconSocialMedia/>
-            <a href="#" className="menuNav" onClick={navToggle}><div></div><div></div><div></div></a>
-        </div>
-        <div className="toggle-nav" id="menuNav">
-            <div className="menuNav-list" >
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/">About</a>
-                    <a href="/">Projects</a>              
-                </nav>               
-            </div>
-        </div>
+        
         <div className="toggle-picture" id="picture">
             <div className="picture-container">
                 <a href="#" onClick={pictureToggle}><span>X</span></a>
