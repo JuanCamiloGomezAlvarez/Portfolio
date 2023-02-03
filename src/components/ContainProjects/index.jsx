638d8c4f29eaa2422a677f51a27ff9b8dde3c255
JuanCamiloGomezAlvarez <kamygz93@hotmail.com>
2023-02-02 19:50:16 -0500

    ajustes en el titulo y la descrpcion de la card

--- src/components/ContainProjects/index.jsx
+++ src/components/ContainProjects/index.jsx
@@ -7,63 +7,73 @@ export default function ContainProjects(){
     const projects = [
         {
             title: "Mokepon",
-            description: "HTML, JS and CSS.",
+            description: "HTML, JavaScript and CSS.",
             link: "https://juancamilogomezalvarez.github.io/mokepon/public/index.html",
-            img: "https://juancamilogomezalvarez.github.io/mokepon/public/img/mokepons_mokepon_ratigueya_attack.png"
+            img: "https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            alt: "pikachu con sombrero marron Foto de Carolina Castilla Arias"
         },
         {
             title: "BatataBit",
             description: "CSS and HTML.",
             link: "https://juancamilogomezalvarez.github.io/Batata-Bit/",
-            img: "https://mir-s3-cdn-cf.behance.net/projects/404/726f1a115332395.Y3JvcCwxOTIwLDE1MDEsMCww.jpg"
+            img: "https://mir-s3-cdn-cf.behance.net/projects/404/726f1a115332395.Y3JvcCwxOTIwLDE1MDEsMCww.jpg",
+            alt: "imagen de un screenshot del proyecto BatataBit"
         },
         {
             title: "Movie App",
-            description: "API REST, JAVASCRIPT, CSS, HTML.",
+            description: "API REST, JavaScript, CSS and HTML.",
             link: "https://juancamilogomezalvarez.github.io/Movie-App/",
-            img: "https://www.androidauthority.com/wp-content/uploads/2019/06/Tubi-best-movie-apps-for-Android.jpg"
+            img: "https://www.androidauthority.com/wp-content/uploads/2019/06/Tubi-best-movie-apps-for-Android.jpg",
+            alt: "screenshot de una app de peliculas"
         },
         {
             title: "Responsive Form",
             description: "CSS AND HTML.",
             link: "https://juancamilogomezalvarez.github.io/responsive-form/",
-            img: "https://www.solucionex.com/sites/default/files/posts/imagen/css_blog.png"
+            img: "https://www.solucionex.com/sites/default/files/posts/imagen/css_blog.png",
+            alt: "logo de css"
         },
         {
-            title: "Componente copia de Twitter follow card",
-            description: " CSS, REACT.",
+            title: "Component copy of Twitter follow card",
+            description: " CSS, React.",
             link: "https://juancamilogomezalvarez.github.io/twitter-follow-card/",
-            img: "https://www.datocms-assets.com/45470/1631110818-logo-react-js.png"
+            img: "https://www.datocms-assets.com/45470/1631110818-logo-react-js.png",
+            alt: "logo de React "
         },
         {
-            title: "Barra de progreso circular",
-            description: " CSS and JAVASCRIPT.",
+            title: "Circular progress bar",
+            description: " CSS and JavaScript.",
             link: " https://juancamilogomezalvarez.github.io/barra-progreso-circular/",
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVC9W3Fw_EZeIX9IlWHxkwfcCs_CsuBTkEAg&usqp=CAU"
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVC9W3Fw_EZeIX9IlWHxkwfcCs_CsuBTkEAg&usqp=CAU",
+            alt: "logos de css, html y javascript"
         },
         {
             title: "Price cards",
-            description: "CSS AND HTML.",
+            description: "CSS and HTML.",
             link: "https://juancamilogomezalvarez.github.io/price-cards/",
-            img: "https://www.solucionex.com/sites/default/files/posts/imagen/css_blog.png"
+            img: "https://www.solucionex.com/sites/default/files/posts/imagen/css_blog.png",
+            alt:"logo de css"
         },
         {
-            title: "Blog CSS y HTML",
+            title: "Blog CSS and HTML",
             description: "CSS and HTML.",
             link: "https://juancamilogomezalvarez.github.io/blog-maquetacion-css/",
-            img: "https://images.pexels.com/photos/7672255/pexels-photo-7672255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            img: "https://images.pexels.com/photos/7672255/pexels-photo-7672255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            alt: "casco de astronauta con fondo morado"
         },
         {
-            title: "Matematicas con JS",
-            description: "JAVASCRIPT.",
+            title: "Math with JS",
+            description: "JavaScript.",
             link: "https://juancamilogomezalvarez.github.io/math-javascript/",
-            img: "https://cdn.pixabay.com/photo/2016/06/13/07/59/pi-1453836_960_720.jpg"
+            img: "https://cdn.pixabay.com/photo/2016/06/13/07/59/pi-1453836_960_720.jpg",
+            alt: "simbolo pi junto a sus primeros numeros decimales"
         },
         {
-            title: "Piedra papel y tijers",
-            description: "HTML, JS and CSS.",
+            title: "Rock paper scissors",
+            description: "HTML and JS .",
             link: "https://juancamilogomezalvarez.github.io/piedra-papel-tijera/",
-            img: "https://media.istockphoto.com/id/1324377846/es/foto/collage-digital-arte-moderno-signo-de-mano-de-roca-tijera-y-papel-con-geometr%C3%ADa-de-conflicto.jpg?s=612x612&w=0&k=20&c=F6aI6jI4yxqkM1qBZFOAKDhKkDpXr5Q49wrggz7LZYQ="
+            img: "https://media.istockphoto.com/id/1324377846/es/foto/collage-digital-arte-moderno-signo-de-mano-de-roca-tijera-y-papel-con-geometr%C3%ADa-de-conflicto.jpg?s=612x612&w=0&k=20&c=F6aI6jI4yxqkM1qBZFOAKDhKkDpXr5Q49wrggz7LZYQ=",
+            alt: "piedra, papel o tijera"
         },
         
     ]
@@ -77,7 +87,9 @@ export default function ContainProjects(){
                                 title = {element.title} 
                                 description = {element.description} 
                                 link = {element.link}
-                                img = {element.img}/>
+                                img = {element.img}
+                                alt = {element.alt}
+                            />
                        
                 })}
             </div>
--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,12 +1,12 @@
 import "./styles.css"
 
-export default function ProjectCard({title, description, link, img}){
+export default function ProjectCard({title, description, link, img, alt}){
     return(
         <>
             <div className="card-container">
                 <div className="card-container-context">
                     <div className="card-container_img">
-                        <img src={img} alt="this is the picture of the porject" />
+                        <img src={img} alt={alt} />
                     </div>
                     <div className="card-container_info">
                         <h4>{title}</h4>
